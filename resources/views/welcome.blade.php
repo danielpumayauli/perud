22b4752aa0952b4fae905bb9d89dc8869a4c78a1
Daniel Pumayauli <danielpumayauli@gmail.com>
2017-12-09 14:37:17 -0500

    Añadido de estilos css al login

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -69,17 +69,17 @@
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
-                        <a href="{{ url('/home') }}">Home</a>
+                        <a href="{{ url('/home') }}">Inicio</a>
                     @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
+                        <a href="{{ route('login') }}">Acceder</a>
+                        <a href="{{ route('register') }}">Registro</a>
                     @endauth
                 </div>
             @endif
 
             <div class="content">
                 <div class="title m-b-md">
-                    PERUD
+                    AQUAVIP
                 </div>
 
                 <div class="links">
